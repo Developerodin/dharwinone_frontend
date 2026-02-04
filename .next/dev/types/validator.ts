049374cc1b4f23401be3c66f4d88328a4b24a9c6
Nishant Jain <nishantjain@Nishants-MacBook-Pro.local>
2026-02-04 14:11:24 +0530

    up

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -1395,6 +1395,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../../app/(components)/(contentlayout)/settings/users/edit/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/settings/users/edit">> = Specific
+  const handler = {} as typeof import("../../../app/(components)/(contentlayout)/settings/users/edit/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/(components)/(contentlayout)/settings/users/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/settings/users">> = Specific
@@ -1786,6 +1795,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
 {
   type __IsExpected<Specific extends AppPageConfig<"/">> = Specific
   const handler = {} as typeof import("../../../app/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
+// Validate ../../../app/roles/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/roles">> = Specific
+  const handler = {} as typeof import("../../../app/roles/page.js")
   type __Check = __IsExpected<typeof handler>
   // @ts-ignore
   type __Unused = __Check
